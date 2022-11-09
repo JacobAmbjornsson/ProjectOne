70514849a692b445d6d6339160c1e9b5de917ebc
JacobAmbjörnsson <jacob.ambjornsson@nbrightstraining.com>
2022-11-09 12:49:14 +0100

    Added .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a new feature with a button");
+        System.out.println("Message from new project 2.0");
     }
 }
