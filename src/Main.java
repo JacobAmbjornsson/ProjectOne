9e8b65dbfd4a4db3de505d3c4b27f84fa43178a7
JacobAmbjörnsson <jacob.ambjornsson@nbrightstraining.com>
2022-11-09 12:27:46 +0100

    New branch with text This is a new feature number 1 from orginal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Change number 2 from original plus change number 2 from clone");
+        System.out.println("This is a new feature number 1 from original");
     }
 }
