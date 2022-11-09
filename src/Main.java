1cb57e56ee75e63325fc8cb284e9ff6d107611e9
JacobAmbjörnsson <jacob.ambjornsson@nbrightstraining.com>
2022-11-09 11:53:55 +0100

    Added change number 2 from clone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Change number 1 from clone");
+        System.out.println("Change number 2 from clone");
     }
 }
