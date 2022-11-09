6196d13434712a9f3fd44181113d627d40007c5e
JacobAmbjörnsson <jacob.ambjornsson@nbrightstraining.com>
2022-11-09 12:50:51 +0100

    Added feature form

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Message from new project 2.0");
+        System.out.println("This is a new form");
     }
 }
